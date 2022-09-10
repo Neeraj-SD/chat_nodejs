ecd4a5d09767683d2c41e387f1222a4da6fce12d
Neeraj S D <neerajsd04@gmail.com>
2022-09-11 00:33:54 +0530

    fcm addition to sendMessage

--- event_handlers/message_handler.js
+++ event_handlers/message_handler.js
@@ -1,6 +1,36 @@
 const { Message } = require('../models/message')
 const { User } = require('../models/user')
 
+
+//! FCM SEND MESSAGE
+
+const sendNotification = async (message, user) => {
+    try {
+        const registrationToken = user.fcmToken;
+        // const registrationToken = 'dM7UYYN4Ro-3SP_rDy71wD:APA91bHEYSFyLB_hlbA70XGT0y-iuvHA7eY4nDyeA7KL0Y0m_JbbySwuYom02tbLU9H_wHYxH4r0qBNKpcb4_VwlHz6Cli8_soIfs7AAd1XAllozmCQwct5KQZyTa35AdHjY6Mm9kiry';
+
+
+        const payload = {
+            notification: {
+                body: message.body
+            }
+        };
+
+        admin.messaging().sendToDevice(registrationToken, payload)
+            .then((response) => {
+                // See the MessagingDevicesResponse reference documentation for
+                // the contents of response.
+                console.log('Successfully sent message:', response);
+            })
+            .catch((error) => {
+                console.log('Error sending message:', error);
+            });
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+
 module.exports = (io, socket) => {
     const sendMessage = async (payload, callback) => {
         payload = JSON.parse(payload)
@@ -17,6 +47,7 @@ module.exports = (io, socket) => {
         const user = await User.findById(payload.to)
 
         const result = await io.to(user.socket_id).emit('chat', message)
+        sendNotification(message, user)
         console.log(message.toJSON())
 
         callback(message)
